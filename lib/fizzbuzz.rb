a2c713b086e1b496a6732cc4fb2daf29492a1274
Christopher <dagswebb@gmail.com>
2021-11-11 15:20:14 +0000

    First Commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,11 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "fizzbuzz"
+    return "fizzbuzz"
   elsif number % 3 == 0
-    puts "fizz"
+    return "fizz"
   elsif number % 5 == 0
-    puts "buzz"
+    return "buzz"
   else
-    puts number
+    return number
   end
 end
-
