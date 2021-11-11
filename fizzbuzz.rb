2e01fd24dc27b6e39b6b3b3978c7565879bf2984
Christopher <dagswebb@gmail.com>
2021-11-11 14:48:08 +0000

    Initialise Rspec

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
+  else
+    puts number
+  end
+end
+
